b192545e0c5d37cd228f0493301d1e7aa8d7eb09
Steven Alexander <stevenalexjr@aol.com>
2023-01-24 09:12:33 -0500

    Initial Commit

---
+++ Access Detroit/Access_DetroitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Access_DetroitApp.swift
+//  Access Detroit
+//
+//  Created by Steven Alexander on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Access_DetroitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Access DetroitUITests/Access_DetroitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Access_DetroitUITestsLaunchTests.swift
+//  Access DetroitUITests
+//
+//  Created by Steven Alexander on 1/24/23.
+//
+
+import XCTest
+
+final class Access_DetroitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
