e90e3a9983168fedaad19508cf44357d9a817eaa
stevenalexjr <97680631+stevenalexjr@users.noreply.github.com>
2023-02-08 09:29:12 -0500

    Add files via upload

---
+++ UserView.swift
@@ -0,0 +1,34 @@
+//
+//  UserView.swift
+//  Access Detroit
+//
+//  Created by Steven Alexander on 2/8/23.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    var body: some View {
+        TabView {
+            EmploymentView()
+                .tabItem() {
+                    Label("Employment", systemImage: "briefcase.circle")
+                }
+            HomeView()
+                .tabItem() {
+                    Label("Events", systemImage: "calendar.circle")
+            }
+            TrainingView()
+                .tabItem() {
+                    Label("Training", systemImage:"brain.head.profile")
+                }
+            
+        }
+    }
+}
+
+struct UserView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserView()
+    }
+}
