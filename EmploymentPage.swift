50d91976f0df85b3815d29d571d5bfb45fb72710
stevenalexjr <97680631+stevenalexjr@users.noreply.github.com>
2023-02-08 09:29:12 -0500

    Add files via upload

---
+++ EmploymentPage.swift
@@ -0,0 +1,16 @@
+//
+//  EmploymentPage.swift
+//  Access Detroit
+//
+//  Created by Steven Alexander on 2/8/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct EmploymentView: View {
+    var body: some View {
+        Text("Hi!")
+        
+    }
+}
