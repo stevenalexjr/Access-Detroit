0346cf77bac43bea200d27ee1e71653d47d24779
Steven Alexander <stevenalexjr@aol.com>
2023-02-08 09:41:08 -0500

    New pages and clean code

---
+++ Access Detroit/HomePage.swift
@@ -0,0 +1,15 @@
+//
+//  HomePage.swift
+//  Access Detroit
+//
+//  Created by Steven Alexander on 2/8/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Also Hello!")
+    }
+}
---
+++ Access Detroit/TrainingPage.swift
@@ -0,0 +1,15 @@
+//
+//  TrainingPage.swift
+//  Access Detroit
+//
+//  Created by Steven Alexander on 2/8/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct TrainingView: View {
+    var body: some View {
+        Text("I think you get it by now")
+    }
+}
